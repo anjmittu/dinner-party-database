752b9af8bbee925d39cedd2831062582876dbde7
Anjali Mittu <anjmittu@gmail.com>
2020-02-01 11:43:48 -0500

    added another method

--- dinner_party_database/utils.py
+++ dinner_party_database/utils.py
@@ -12,3 +12,14 @@ class Utils:
             {"number": phone},
             {"$set": {"last_question": n}}
         )
+
+    @staticmethod
+    def get_last_question(phone):
+        client = pymongo.MongoClient(os.getenv("MONGODB_URL"))
+        db = client["diner-party"]
+        col = db["people"]
+        result = col.find_one(
+            {"number": phone},
+            {"last_question": 1}
+        )
+        return result["last_question"]
