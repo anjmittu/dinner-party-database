59659c9811a16090cd3c6b99ffac64239c84b2c0
Anjali Mittu <anjmittu@gmail.com>
2020-02-01 11:29:01 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup(
     name='DinnerPartyDatabase',
     version='1',
     packages=['dinner_party_database',],
-    long_description=open('README.txt').read()
+    long_description=open('README.md').read(),
+    install_requires=[
+       'pymongo',
+        'dnspython'
+    ]
 )
