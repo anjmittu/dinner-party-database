88156dd61d97bdb1a1f7b2b37d91b10dd03cb1bc
Anjali Mittu <anjmittu@gmail.com>
2020-02-02 00:25:00 -0500

    accept more responses

--- dinner_party_database/response_utils.py
+++ dinner_party_database/response_utils.py
@@ -3,7 +3,7 @@ class ResponseUtils:
     def response_sentiment(response):
         if "know" in response.lower() or "idk" in response.lower():
             return 0
-        if "yes" in response.lower():
+        if "yes" in response.lower() or "yee" in response.lower() or "yeah" in response.lower() or "ye" in response.lower():
             return 1
-        if "no" in response.lower():
+        if "no" in response.lower() or "ne" in response.lower():
             return -1
