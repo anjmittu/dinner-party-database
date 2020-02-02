37df164d2350644671641f4982545a12f87cd706
Anjali Mittu <anjmittu@gmail.com>
2020-02-02 00:32:05 -0500

    accept more responses

--- dinner_party_database/response_utils.py
+++ dinner_party_database/response_utils.py
@@ -5,5 +5,7 @@ class ResponseUtils:
             return 0
         if "yes" in response.lower() or "yee" in response.lower() or "yeah" in response.lower() or "ye" in response.lower():
             return 1
+        if "sure" in response.lower() or "liked" in response.lower():
+            return 1
         if "no" in response.lower() or "ne" in response.lower():
             return -1
